f7f3d680e797748132e4ac653b97112df5480981
Polina Balazhy <lefugdot@gmail.com>
2022-10-29 16:24:01 +0300

    set cookies to secure

--- consts/requests.js
+++ consts/requests.js
@@ -1,13 +1,13 @@
-const ACCESS_TOKEN = "accessToken";
+const tokens = { ACCESS_TOKEN: "accessToken", REFRESH_TOKEN: "refreshToken" };
 
 const ONE_DAY = 86400000;
 
 const COOKIE_OPTIONS = {
   maxAge: ONE_DAY,
   httpOnly: true,
-  secure: false,
+  secure: true,
   sameSite: "none",
   domain: "localhost",
 };
 
-module.exports = { ACCESS_TOKEN, COOKIE_OPTIONS };
+module.exports = { tokens, COOKIE_OPTIONS };
